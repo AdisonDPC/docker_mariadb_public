e135f0db9f4bc993b9d84d4dba1f3411391f7e03
Adison Daniel Pérez Caraballo <adisondpc@gmail.com>
2019-05-23 15:44:44 +0100

    Update Commit - 23/05/2019 - Update Scripts 09.

--- config/mariadb/script.sql
+++ config/mariadb/script.sql
@@ -4,23 +4,25 @@
 
 USE db_test;
 
--- CREATE TABLE `users` (
---     `id` int(11) NOT NULL,
---     `name` varchar(250) NOT NULL,
---     `email` varchar(250) NOT NULL,
---     `password` varchar(250) NOT NULL,
---     `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
---     `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `users` (
+    `id` int(11) NOT NULL,
+    `name` varchar(250) NOT NULL,
+    `email` varchar(250) NOT NULL,
+    `password` varchar(250) NOT NULL,
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
 --
 
--- INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `updated_at`) VALUES
---     (1, 'shahroze nawaz', 'shahroze.nawaz@adpcprojects.com', '', '2017-08-06 11:47:24', '0000-00-00 00:00:00'),
---     (2, 'Pere Hospital', 'pere@adpcprojects.com', '', '2017-08-06 16:43:12', '0000-00-00 00:00:00'),
---     (3, 'jane doe', 'janedoe@adpcprojects.com', 'iamstupid', '2017-08-06 17:06:57', '2017-08-06 17:06:57');
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `updated_at`) VALUES
+    (1, 'John Doe 01', 'johndoe01@adpcprojects.com', 'adpcprojects', '2017-08-06 11:47:24', '2017-08-06 17:06:57'),
+    (2, 'John Doe 02', 'johndoe02@adpcprojects.com', 'adpcprojects', '2017-08-06 16:43:12', '2018-08-06 17:06:57'),
+    (3, 'John Doe 03', 'johndoe03@adpcprojects.com', 'adpcprojects', '2017-08-06 17:06:57', '2017-08-06 17:06:57'),
+    (4, 'John Doe 04', 'johndoe04@adpcprojects.com', 'adpcprojects', '2017-08-06 17:06:57', '2017-08-06 17:06:57'),
+    (5, 'John Doe 05', 'johndoe05@adpcprojects.com', 'adpcprojects', '2017-08-06 17:06:57', '2016-08-06 17:06:57');
 
--- ALTER TABLE `users` ADD PRIMARY KEY (`id`);
--- ALTER TABLE `users` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `users` ADD PRIMARY KEY (`id`);
+ALTER TABLE `users` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
