c07bd0189b3e5e9ae80b92f74ec494c273d442f2
Adison Daniel Pérez Caraballo <adisondpc@gmail.com>
2019-05-23 09:32:56 +0100

    Update Commit - 23/05/2019 - Update Scripts 07.

--- monitor.sh
+++ monitor.sh
@@ -5,3 +5,12 @@ AUTHOR=adpc
 NAME=mariadb
 TAG=1.0
 PORT_3306=3344
+
+MYSQL_ROOT_PASSWORD=testuser@docker
+MYSQL_USER=testuser
+MYSQL_PASSWORD=testuser@docker
+MYSQL_DATABASE=db_test
+
+# docker exec -it ${AUTHOR}_$(echo "${NAME}" | tr '/' '_') bash -c "mysql -u ${MYSQL_USER} -p${MYSQL_PASSWORD}"
+
+docker exec -it ${AUTHOR}_$(echo "${NAME}" | tr '/' '_') bash
