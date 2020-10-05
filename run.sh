496e2e2f30f09cb16b6c6a29fa408eca5a5644bc
Adison Daniel Pérez Caraballo <adisondpc@gmail.com>
2020-10-05 12:40:38 +0100

    Update Commit - 05/10/2020 - Update Files 01.

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ NAME=mariadb
 TAG=latest
 PORT_3306=3344
 USER=${USER}
-HOME_PATH=${HOME} 
+HOME_PATH=${HOME}
 PWD_PATH=$(pwd)
 READ_LINK=$(readlink -f $0)
 DIR_NAME=$(dirname ${READ_LINK})
@@ -14,6 +14,7 @@ ROOT_PATH=${DIR_NAME}
 LOG_PATH=${ROOT_PATH}/logs
 DATA_PATH=${ROOT_PATH}/data
 CONFIG_PATH=${ROOT_PATH}/config
+SCRIPT_PATH=${ROOT_PATH}/script
 
 MYSQL_ROOT_PASSWORD=testuser@docker
 MYSQL_USER=testuser
@@ -25,25 +26,12 @@ MYSQL_RANDOM_ROOT_PASSWORD=no
 # Run docker container.
 
 # Use in Development.
-docker run --name ${AUTHOR}_$(echo "${NAME}" | tr '/' '_') -it --rm \
-    -p ${PORT_3306}:3306 \
-    -v ${CONFIG_PATH}/mariadb/my.cnf:/etc/mysql/conf.d/my.cnf \
-    -v ${CONFIG_PATH}/mariadb/script.sql:/script.sql \
-    -v ${DATA_PATH}:/var/lib/mysql \
-    -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
-    -e MYSQL_USER=${MYSQL_USER} \
-    -e MYSQL_PASSWORD=${MYSQL_PASSWORD} \
-    -e MYSQL_DATABASE=${MYSQL_DATABASE} \
-    -e MYSQL_ALLOW_EMPTY_PASSWORD=${MYSQL_ALLOW_EMPTY_PASSWORD} \
-    -e MYSQL_RANDOM_ROOT_PASSWORD=${MYSQL_RANDOM_ROOT_PASSWORD} \
-    ${NAME}:${TAG}
-
-# Use in Production.
-# docker run --name ${AUTHOR}_$(echo "${NAME}" | tr '/' '_') -d -it --rm \
+# docker run --name ${AUTHOR}_$(echo "${NAME}" | tr '/' '_') -it --rm \
 #     -p ${PORT_3306}:3306 \
 #     -v ${CONFIG_PATH}/mariadb/my.cnf:/etc/mysql/conf.d/my.cnf \
 #     -v ${CONFIG_PATH}/mariadb/script.sql:/script.sql \
 #     -v ${DATA_PATH}:/var/lib/mysql \
+#     -v ${SCRIPT_PATH}:/script \
 #     -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
 #     -e MYSQL_USER=${MYSQL_USER} \
 #     -e MYSQL_PASSWORD=${MYSQL_PASSWORD} \
@@ -51,3 +39,27 @@ docker run --name ${AUTHOR}_$(echo "${NAME}" | tr '/' '_') -it --rm \
 #     -e MYSQL_ALLOW_EMPTY_PASSWORD=${MYSQL_ALLOW_EMPTY_PASSWORD} \
 #     -e MYSQL_RANDOM_ROOT_PASSWORD=${MYSQL_RANDOM_ROOT_PASSWORD} \
 #     ${NAME}:${TAG}
+
+# Use in Production.
+# docker run --name ${AUTHOR}_$(echo "${NAME}" | tr '/' '_') -d -it --rm \
+#     -p ${PORT_3306}:3306 \
+#     -v ${CONFIG_PATH}/mariadb/my.cnf:/etc/mysql/conf.d/my.cnf \
+#     -v ${CONFIG_PATH}/mariadb/script.sql:/script.sql \
+#     -v ${DATA_PATH}:/var/lib/mysql \
+#     -v ${SCRIPT_PATH}:/script \
+#     -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
+#     -e MYSQL_USER=${MYSQL_USER} \
+#     -e MYSQL_PASSWORD=${MYSQL_PASSWORD} \
+#     -e MYSQL_DATABASE=${MYSQL_DATABASE} \
+#     -e MYSQL_ALLOW_EMPTY_PASSWORD=${MYSQL_ALLOW_EMPTY_PASSWORD} \
+#     -e MYSQL_RANDOM_ROOT_PASSWORD=${MYSQL_RANDOM_ROOT_PASSWORD} \
+#     ${NAME}:${TAG}
+
+docker run --name ${AUTHOR}_$(echo "${NAME}" | tr '/' '_') -d -it --rm \
+    -p ${PORT_3306}:3306 \
+    -v ${CONFIG_PATH}/mariadb/my.cnf:/etc/mysql/conf.d/my.cnf \
+    -v ${CONFIG_PATH}/mariadb/script.sql:/SCRIPT.sql \
+    -v ${DATA_PATH}:/var/lib/mysql \
+    -v ${SCRIPT_PATH}:/script \
+    -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
+    ${NAME}:${TAG}
